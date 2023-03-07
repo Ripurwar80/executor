e523c714ea971c12da8389583bb0c8de1c879594
Rishabh Purwar <RishabhPurwar@fico.com>
2023-03-08 04:18:53 +0530

    p

--- src/engine/executor.sh
+++ src/engine/executor.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pwd
-curl -o execution.yaml http://localhost:3001/api/v1/file/configuration.yaml
-curl -o loadscenario.yaml http://localhost:3001/api/v1/file/load-scenario.yaml
+curl -o execution1.yaml http://35.86.80.21:3001/api/v1/file/configuration.yaml
+curl -o loadscenario.yaml http://35.86.80.21:3001/api/v1/file/load-scenario.yaml
 java -version
 javac -version
 bzt execution.yaml loadscenario.yaml
