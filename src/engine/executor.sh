5cbf90abae49241477dc27fd705a9d075938f511
Ripurwar80 <34250719+Ripurwar80@users.noreply.github.com>
2023-03-14 17:50:00 +0530

    Update executor.sh

--- src/engine/executor.sh
+++ src/engine/executor.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 pwd
-curl -o execution1.yaml http://35.86.80.21:3001/api/v1/file/configuration.yaml
-curl -o loadscenario.yaml http://35.86.80.21:3001/api/v1/file/load-scenario.yaml
-java -version
-javac -version
+curl -o execution1.yaml http://54.197.206.152:3001/api/v1/file/configuration.yaml
+curl -o loadscenario.yaml http://54.197.206.152:3001/api/v1/file/load-scenario.yaml
 bzt execution.yaml loadscenario.yaml
